9b353330ff591745b6d3aa9534f5f10ff86d5026
oudsoft <oudsoft@gmail.com>
2022-11-05 09:13:29 +0700

    fix

--- public/report-design/report-generator.js
+++ public/report-design/report-generator.js
@@ -123,7 +123,7 @@ function doMergeContent(elements, variable, qrcodeLink, caseId, rsH, cb){
     */
 
     let endResultAt = parseFloat(rsH) + parseFloat(resultTopPosition); // 1380;
-    let eff = 0.5;
+    let eff = 0.3;
     endResultAt += (endResultAt * eff);
     console.log('endResultAt=>' + endResultAt);
 
