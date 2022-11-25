97b9e7c0a584ecb0fb0ec2bf8ff9bb38825040c2
oudsoft <oudsoft@gmail.com>
2022-11-26 04:55:03 +0700

    fix

--- public/radio/bundle.js
+++ public/radio/bundle.js
@@ -6968,7 +6968,7 @@ module.exports = function ( jq ) {
 			success: function(data){
 				let stremLink = URL.createObjectURL(new Blob([data], {type: 'application/octetstream'}));
 				pom.setAttribute('href', stremLink);
-				pom.setAttribute('download', caseDicomZipFilenameb);
+				pom.setAttribute('download', caseDicomZipFilename);
 				pom.click();
 				$.notify(('ดาวน์โหลดไฟล์ ' + caseDicomZipFilename + ' เสร็จสมบูรณ์'), 'info' );
 			}
