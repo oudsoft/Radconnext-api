93572dff971ea1df1ab60cacd397d51e5e642783
oudsoft <oudsoft@gmail.com>
2022-11-26 11:12:41 +0700

    re-fix

--- public/radio/bundle.js
+++ public/radio/bundle.js
@@ -4870,7 +4870,7 @@ module.exports = function ( jq ) {
 			let eventData = common.doCreateOpenCaseData(caseItem);
 			let currentCaseRes = await common.doGetApi('/api/cases/status/' + caseItem.id, {});
 			if (currentCaseRes.current == 2){
-				/*
+
 				let nextCaseStatus = 8;
 				let radioName = userdata.userinfo.User_NameTH + ' ' + userdata.userinfo.User_LastNameTH;
 				let actionRemark = 'รังสีแพทย์ ' + radioName + ' เปิดเคสสำเร็จ [web]'
@@ -4882,12 +4882,13 @@ module.exports = function ( jq ) {
 				} else {
 					$.notify('เกิดข้อผิดพลาด ไม่สามารถอัพเดทสถานะเคสได้ในขณะนี้', 'error');
 				}
-				*/
 
+				/*
 				let nextCaseStatus = 8;
 				eventData.statusId = nextCaseStatus;
 				eventData.startDownload = 0;
 				$(openCmd).trigger('opencase', [eventData]);
+				*/
 			} else if ((currentCaseRes.current == 8) || (currentCaseRes.current == 9) || (currentCaseRes.current == 14)){
 				eventData.statusId = caseItem.casestatusId;
 				eventData.startDownload = 0;
