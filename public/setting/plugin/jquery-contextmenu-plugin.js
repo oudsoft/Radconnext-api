00642df253c167916c6f606ace4f540abe62bf2b
oudsoft <oudsoft@gmail.com>
2023-01-20 20:17:30 +0700

    update contextmenu

--- public/setting/plugin/jquery-contextmenu-plugin.js
+++ public/setting/plugin/jquery-contextmenu-plugin.js
@@ -3,21 +3,25 @@
   $.fn.contextmenu = function( options ) {
 
     var settings = $.extend({
-      menuItems: []
+      //menuItems: []
     }, options );
 
     let $this = this;
     let containerFrame = $('<table width="100%" border="0" cellspacing="0" cellpadding="2"></table>');
     let containerStyle = {'display': 'none', 'position': 'absolute', 'border': '1px solid black', 'border-radius': '3px', 'min-width': '200px', 'background-color': 'white', 'box-shadow': '10px 10px 5px #888888'};
-    let iconStyle = {width: '40px', height: 'auto'};
+    let iconStyle = {width: '30px', height: 'auto'};
 
     const doAddMenuItem = function(item) {
       let itemRow = $('<tr></tr>');
       $(itemRow).hover(()=>{
         $(itemRow).css({'cursor': 'pointer', 'background-color': '#2579B8', 'color': 'white'});
+      }, ()=>{
+        $(itemRow).css({'cursor': '', 'background-color': '', 'color': ''});
       });
       $(itemRow).on('click', (evt)=>{
-        item.callback();
+        if (item.callback) {
+          item.callback();
+        }
       });
       let iconCmd = $('<img/>').attr('src', item.iconUrl).css(iconStyle);
       let iconCol = $('<td width="50px" align="center"></td>').append($(iconCmd));
@@ -27,19 +31,12 @@
     }
 
     const init = function() {
-      /*
-      let containerBox = $('<div></div>');
-      $(containerBox).css(containerStyle);
-      */
       if (settings.menuItems.length > 0) {
         for (let i=0; i<settings.menuItems.length; i++) {
           let menuItem = settings.menuItems[i];
           doAddMenuItem(menuItem);
         }
       }
-      /*
-      return $(containerBox).append($(containerFrame));
-      */
       return $(containerFrame);
     }
 
